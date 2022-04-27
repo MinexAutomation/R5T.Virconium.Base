e32e294904a23bc53d3e2a4092e6771f1e5a7277
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:34 -0700

    Interim changes.

---
+++ source/R5T.Virconium.Base/Code/Services/Definitions/IVirconiumService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+using R5T.T0064;
+
+
+namespace R5T.Virconium
+{
+    [ServiceDefinitionMarker]
+    public interface IVirconiumService : IServiceDefinition
+    {
+        /// <summary>
+        /// Determine all recursive dependencies of all project references in a solution, and add those dependencies to the solution if missing.
+        /// </summary>
+        void AddMissingProjectDependencies(string solutionFilePath, TextWriter writer, bool dryRun = true);
+        /// <summary>
+        /// Determine all recursive dependencies of the non-dependency project references in a solution, and remove any dependencies that are not on that list.
+        /// </summary>
+        void RemoveExtraneousDependencies(string solutionFilePath, TextWriter writer, bool dryRun = true);
+    }
+}
